0e907770c832f5fa0ff70dccc1ff7718c2f31eb2
owpao <johnpaoloflores@gmail.com>
2019-04-10 20:06:05 -0700

    changes

--- web/src/App.js
+++ web/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <code>Hello World, I am owpao.</code>
           </p>
           <a
             className="App-link"
