b998cb68eaa793243363f8c9da18c94e268e8325
owpao <johnpaoloflores@gmail.com>
2019-04-10 23:05:50 -0700

    changes

--- web/src/components/profile.js
+++ web/src/components/profile.js
@@ -4,8 +4,9 @@ import pic from '../assets/pao.jpg'
 class Profile extends Component {
   render() {
     return (
-        <div>
+        <div style={{justifyContent:'space-around'}}>
             <img src={pic} alt='a picture' style={{height: 450}}/>
+            <div> ano na </div>
         </div>
     );
   }
