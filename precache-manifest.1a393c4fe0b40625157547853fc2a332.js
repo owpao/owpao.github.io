aa8acafe6a9f2c67af46f3350c5d1383c29fd005
owpao <johnpaoloflores@gmail.com>
2019-04-10 20:56:31 -0700

    Updates

---
+++ precache-manifest.1a393c4fe0b40625157547853fc2a332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "33ed4ec846e9d84e8327",
+    "url": "/web/static/js/runtime~main.c75ffd02.js"
+  },
+  {
+    "revision": "529ced0bfb12a0063f02",
+    "url": "/web/static/js/main.12024f67.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/web/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "529ced0bfb12a0063f02",
+    "url": "/web/static/css/main.949a7bf1.chunk.css"
+  },
+  {
+    "revision": "1cef4696d3030690f24aa8c9d3e6ed12",
+    "url": "/web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/owpao.github.io/precache-manifest.571597fbf5c648368f4517952da943a3.js"
+  "/web/precache-manifest.1a393c4fe0b40625157547853fc2a332.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/owpao.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/web/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
