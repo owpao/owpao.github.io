79bc67803c330f2692d16110248adfa1a0e23ab5
owpao <johnpaoloflores@gmail.com>
2019-04-16 23:39:27 -0700

    layout changes

--- web/src/App.js
+++ web/src/App.js
@@ -3,14 +3,22 @@ import React, { Component } from 'react';
 import './App.css';
 import './materialize/css/materialize.css'
 import Profile from './components/profile';
+import { AppBar, Grid } from '@material-ui/core';
+// import { grey } from '@material-ui/core/colors';
 
 class App extends Component {
   render() {
     return (
-      <div class='container' style={{ justifyContent:'space-around'}}>
-      <Profile/>
-      </div>
-      
+      <Grid container spacing={16} style={{height:'100%'}}>
+        < Grid item style={{display:'flex', alignContent:'center', justifyContent:'center'}}>
+            {/* <AppBar color="default" style={{alignItems:'center'}}>Test</AppBar>  */}
+          </Grid>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              < Profile/>
+            </Grid>
+          </Grid>
+      </Grid>
     );
   }
 }
--- web/src/components/profile.js
+++ web/src/components/profile.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import pic from '../assets/pao.jpg'
+import pic from '../assets/pao.jpg' 
+import { Paper, Grid } from '@material-ui/core';
+
+const styles = {
+  paper: {
+    width:250,
+    height: 250,
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex'
+  }
+}
 
 class Profile extends Component {
   render() {
     return (
-        <div style={{justifyContent:'space-around'}}>
-            <img src={pic} alt='a picture' style={{height: 450}}/>
-            <div> ano na </div>
-        </div>
+      <Grid container spacing={16} justify='center'>
+        <Grid item>
+          <Paper style={styles.paper}>
+            <img src={pic} alt='pic ko baket' height={150}/>
+          </Paper>
+        </Grid>
+        <Grid item>
+          <Paper style = {styles.paper}>
+            owpao
+          </Paper>
+        </Grid>
+      </Grid>
     );
   }
 }
