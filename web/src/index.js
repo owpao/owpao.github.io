a8a7970d849a0c2e01df3a9983d80bf2b466c275
owpao <johnpaoloflores@gmail.com>
2019-04-28 15:41:16 -0700

    added routing and navbar + icon; animated text for some pizzazzzzz

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import App from './App';
+import PageNotFound from './components/notfound'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/owpao-io" component={App} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    </Router>
+  )
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
