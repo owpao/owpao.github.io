a40e6614e1644ab1c84c678eb4770093b82caa65
owpao <johnpaoloflores@gmail.com>
2019-04-28 15:41:16 -0700

    added routing and navbar + icon; animated text for some pizzazzzzz

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import './materialize/css/materialize.css'
-import Profile from './components/profile';
-import NavigationBar from './components/appbar';
-// import { grey } from '@material-ui/core/colors';
-
+import React, { Component } from "react";
+import "./App.css";
+import "./materialize/css/materialize.css";
+import Profile from "./components/profile";
+import NavigationBar from "./components/appbar";
 
 class App extends Component {
   render() {
     return (
-      <div style={{width:'100%', height: '100%', display:'flex', justifyContent:'center'}}>
-        <div style={{flexDirection:'row'}}>
-          <NavigationBar/>
-        </div>
-        <div style={{flexDirection:'row'}}>
-          <Profile/>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
+        <div style={{ flexDirection: "row", width: "80%" }}>
+          <div style={{ marginTop: 20 }}>
+            <NavigationBar />
+          </div>
+          <div style={{ height:'100%', marginTop: 100 }}>
+            <Profile />
+          </div>
+
+          <div className='footer'
+            style={{
+              display:'flex',
+              flexDirection:'column',
+              flex: 1,
+              justifyContent: 'space-between',
+              textAlign:'center',
+              marginBottom: 36,
+              // height:'100%'
+            }}
+          >
+            "Kabado as a person"
+          </div>
         </div>
       </div>
     );
--- web/src/components/profile.js
+++ web/src/components/profile.js
@@ -1,31 +1,47 @@
-import React, { Component } from 'react';
-import pic from '../assets/pao.jpg' 
-import { Paper } from '@material-ui/core';
+import React, { Component } from "react";
+import pic from "../assets/pao.jpg";
+import { Paper, Typography } from "@material-ui/core";
+import TextLoop from "react-text-loop";
 
 const styles = {
   paper: {
-    width:250,
+    width: 250,
     height: 250,
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    marginLeft:10,
-    marginRight:10
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    marginLeft: 10,
+    marginRight: 10
   }
-}
+};
 
 class Profile extends Component {
   render() {
     return (
-      <div style={{alignContent:'center', display:'flex',flexDirection:'row', width:'100%', height: '100%', justifyContent:'center', alignItems:'center'}}>
+      <div
+        style={{
+          alignContent: "center",
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
         <div>
           <Paper style={styles.paper}>
-            <img src={pic} alt='pic ko baket' height={150}/>
+            <img src={pic} alt="pic ko baket" height={150} />
           </Paper>
         </div>
         <div>
-          <Paper style={styles.paper}>
-            owpao
+          <Paper style={Object.assign("", styles.paper, { width: 500 })}>
+              <Typography variant="h6" color="inherit" style={{textAlign:'center'}}>
+                <TextLoop noWrap={false}>
+                      <div style={{width:250}}>Hello World</div>
+                      <div style={{width:250}}>Paolo Flores</div>
+                  </TextLoop>
+              </Typography>
           </Paper>
         </div>
       </div>
