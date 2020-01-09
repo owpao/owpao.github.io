14bfd782575b26d6831013b6bdbba233b4a0da02
owpao <johnpaoloflores@gmail.com>
2020-01-09 23:58:18 +0800

    update github pages

--- web/src/components/notfound.js
+++ web/src/components/notfound.js
@@ -3,7 +3,14 @@ import React, { Component } from "react";
 class PageNotFound extends Component{
     render(){
         return (
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height:'100vh', fontSize:'3vw', color: '#bdbdbd'}}>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                fontSize: '3vw',
+                color: '#bdbdbd'
+            }}>
                 Page not found :(
             </div>
         )
