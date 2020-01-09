58f014c536b3658bf16f520dfa50933febe7f9df
owpao <johnpaoloflores@gmail.com>
2020-01-10 00:19:31 +0800

    Updates

---
+++ precache-manifest.5e9d1132f2a2514f75cf2b815bb03e68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "335d87610062c14938d5",
+    "url": "/owpao-io/static/js/runtime~main.c06d492e.js"
+  },
+  {
+    "revision": "6e444791aff3fecc7316",
+    "url": "/owpao-io/static/js/main.93a3ae1b.chunk.js"
+  },
+  {
+    "revision": "4f0a1298d484729fa2da",
+    "url": "/owpao-io/static/js/2.34bb56cb.chunk.js"
+  },
+  {
+    "revision": "6e444791aff3fecc7316",
+    "url": "/owpao-io/static/css/main.c833da9a.chunk.css"
+  },
+  {
+    "revision": "dd5945c3b324f3eed3ed715593023ec9",
+    "url": "/owpao-io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32ee612ed39200f7b42459db71dc30ef.js"
+  "/owpao-io/precache-manifest.5e9d1132f2a2514f75cf2b815bb03e68.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/owpao-io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
