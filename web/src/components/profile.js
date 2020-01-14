1faaea7362e2394e273f0ef3a91734d96b0d539f
John Paolo Flores <john.paolo.flores@oocl.com>
2020-01-14 16:33:24 +0800

    changed picture

--- web/src/components/profile.js
+++ web/src/components/profile.js
@@ -1,50 +1,46 @@
-import React, { Component } from "react";
-import { Paper, Typography } from "@material-ui/core";
+import React, {Component} from "react";
+import {Paper, Typography} from "@material-ui/core";
 import TextLoop from "react-text-loop";
 
 const styles = {
-  paper: {
-    width: 250,
-    height: 250,
-    alignItems: "center",
-    justifyContent: "center",
-    display: "flex",
-    marginLeft: 10,
-    marginRight: 10
-  }
+    paper: {
+        justifyContent: "center",
+        alignItems: 'center',
+        display: "flex",
+        marginLeft: 10,
+        marginRight: 10,
+        padding: 10
+    }
 };
 
 class Profile extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          alignContent: "center",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <div>
-          <Paper style={styles.paper}>
-            <img src={'pao.jpg'} alt="my pic" height={150} />
-          </Paper>
-        </div>
-        <div>
-          <Paper style={Object.assign("", styles.paper, { width: 500 })}>
-              <Typography variant="h6" color="inherit" style={{textAlign:'center'}}>
-                <TextLoop noWrap={false}>
-                      <div style={{width:500}}>Hello World</div>
-                      <div style={{width:500}}>I'm John Paolo Flores</div>
-                      <div style={{width:500}}>Web Developer at OOCL Philippines Inc.</div>
-                  </TextLoop>
-              </Typography>
-          </Paper>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center"
+                }}
+            >
+                <div>
+                    <Paper style={styles.paper}>
+                        <img src={'pao.jpg'} alt="my pic" height={300}/>
+                    </Paper>
+                </div>
+                <div style={{display:'flex'}}>
+                    <Paper style={Object.assign("", styles.paper, {width: 500})}>
+                        <Typography variant="h6" color="inherit" style={{textAlign: 'center'}}>
+                            <TextLoop noWrap={false}>
+                                <div style={{width: 500}}>Hello World</div>
+                                <div style={{width: 500}}>I'm John Paolo Flores</div>
+                                <div style={{width: 500}}>Web Developer at OOCL Philippines Inc.</div>
+                            </TextLoop>
+                        </Typography>
+                    </Paper>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Profile;
