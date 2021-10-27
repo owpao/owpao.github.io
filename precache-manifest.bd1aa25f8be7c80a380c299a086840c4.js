8506da0f3dc6c5fc021d4e96461e00ba11ed5cf3
owpao <johnpaoloflores@gmail.com>
2021-10-28 00:19:22 +0800

    Updates

---
+++ precache-manifest.bd1aa25f8be7c80a380c299a086840c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a20594f1ff03ff093a7e7f9f0c18f7fa",
+    "url": "/static/media/bnwbg1.a20594f1.jpg"
+  },
+  {
+    "revision": "cda1af62cb6a7bf5051a",
+    "url": "/static/css/main.97377892.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7492ec0a070638147c5",
+    "url": "/static/js/2.0dd19336.chunk.js"
+  },
+  {
+    "revision": "315f8d376e347ac7e18bb61908c8c46e",
+    "url": "/static/media/pao.315f8d37.jpg"
+  },
+  {
+    "revision": "cda1af62cb6a7bf5051a",
+    "url": "/static/js/main.a281541f.chunk.js"
+  },
+  {
+    "revision": "6f1dec00c416c0c1794e3d6795edb4fc",
+    "url": "/static/media/bnwbg3.6f1dec00.jpg"
+  },
+  {
+    "revision": "ab8154710fdbe233ae88eda64ae1ebaa",
+    "url": "/static/media/ulysses-donation-drive-preview.ab815471.jpg"
+  },
+  {
+    "revision": "73d5b09e2e4292c4d071b049aed29f2a",
+    "url": "/static/media/stikwel-preview.73d5b09e.jpg"
+  },
+  {
+    "revision": "d9f1f532d8f59bfd447f76d4e4145baf",
+    "url": "/static/media/go-papa-logo.d9f1f532.png"
+  },
+  {
+    "revision": "710a54ad6fe381bcbb0eb0bfe586c6c8",
+    "url": "/index.html"
+  }
+];
