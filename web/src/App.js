69e99671274d27df3da74217cc823b03a3535580
owpao <johnpaoloflores@gmail.com>
2021-10-27 23:44:57 +0800

    update project

--- web/src/App.js
+++ web/src/App.js
@@ -7,30 +7,33 @@ import {GoMail} from "react-icons/go"
 import {AiOutlineStar} from "react-icons/ai"
 import BG1 from "./assets/bnwbg1.jpg"
 import BG3 from "./assets/bnwbg3.jpg"
-import PROF_PIC from "./assets/pao.jpg"
 import ProjectItem from "./components/projectitem";
-import {FaJava, FaReact, FaPython, FaDocker, FaSass} from "react-icons/fa"
-import {SiSpringboot, SiMysql, SiJest, SiGit} from "react-icons/si"
-import { Grid } from "@material-ui/core";
+import {FaJava, FaReact, FaPython, FaDocker, FaSass, FaLinkedin} from "react-icons/fa"
+import {SiSpringboot, SiMysql, SiJest, SiGit, SiGithub} from "react-icons/si"
+import { Button, Grid } from "@material-ui/core";
+
+import ULYSSES_DONATION_PREVIEW from "./assets/ulysses-donation-drive-preview.jpg"
+import STIKWEL_PREVIEW from "./assets/stikwel-preview.jpg"
+import GO_PAPA_LOGO from "./assets/go-papa-logo.png"
 
 
 const PORTFOLIO = [
     {
-        previewPicture: PROF_PIC,
+        previewPicture: ULYSSES_DONATION_PREVIEW,
         projectTitle: '"No help too little" donation drive',
-        projectDescription: "Quick Webpage to show transparency of donations made for a company making adhesives using ReactJS as frontend and google spreadsheet API",
+        projectDescription: "Quick Webpage to show transparency of donations made for a company making adhesives using ReactJS as frontend and tabletop package for retrieving google spreadsheet data",
         projectLink: "https://ulysses-donation-drive.netlify.app"
     },
     {
-        previewPicture: PROF_PIC,
+        previewPicture: STIKWEL_PREVIEW,
         projectTitle: "Stikwel",
         projectDescription: "Product Website made for a company making adhesives using ReactJS as frontend and Firebase/NodeJS for backend",
         projectLink: "https://stikwel-prd.web.app/"
     },
     {
-        previewPicture: PROF_PIC,
+        previewPicture: GO_PAPA_LOGO,
         projectTitle: "GoPapa",
-        projectDescription: "Website offering food delivery service for citizens of Calamba, Laguna",
+        projectDescription: "Website offering food delivery service for citizens of Calamba, Laguna. Built using ReactJS for frontend and Firebase/NodeJS for backend.",
         projectLink: "http://go-papa.com"
     },
 
@@ -38,6 +41,14 @@ const PORTFOLIO = [
 
 
 const App = ()=> {
+
+    const onLinkedInClick = () => {
+        window.open("https://www.linkedin.com/in/owpaoo/")
+    }
+    
+    const onGitHubClicked = () => {
+        window.open("https://www.github.com/owpao/")
+    }
 
     return (
         <div
@@ -60,21 +71,19 @@ const App = ()=> {
                 </Parallax>
 
                 <div style={{padding: "20px"}}>
-                    <h1 className='header' style={{color:"white"}}> 
+                    <h1 className='header' style={{color:"white", display:'flex', justifyContent:'center'}}> 
                         <AiOutlineStar size={40} style={{color:'#BDBDBD', verticalAlign: 'sub', margin: '0 10px'}}/> 
                         Projects Handled
+                        <AiOutlineStar size={40} style={{color:'#BDBDBD', verticalAlign: 'sub', margin: '0 10px'}}/> 
                         </h1>
                     <div style={{padding: "0px 100px"}}>
                         {PORTFOLIO.map((project, index)=>{return <ProjectItem key={index} {...project} />})}
                     </div>
                 </div>
 
-                <Parallax
-                    bgImage={BG3}
-                    bgImageAlt="the 2nd bg"
-                    strength={300}
-                >
-                    <div style={{ height: 400, margin: '75px 0px'}}>
+                <div style={{backgroundColor: "white", padding:"50px 0"}}>
+
+                    <div style={{margin: '75px 0px'}}>
                         <h1 className="header" style={{display:'flex', justifyContent:'center', marginBottom: 50}}>
                             Skills | Technologies
                         </h1>
@@ -91,22 +100,32 @@ const App = ()=> {
                             <div style={{width:100}}><SiGit size={'100%'}/> </div>
                         </Grid>
                     </div>
-                </Parallax>
-
-
-
-                <div style={{display:'flex', backgroundColor: "white", justifyContent:'center'}}>
-                    <div style={{marginRight: 30}}>
-                        <h1 className="header">
-                            <GoMail size={40} style={{color:'#BDBDBD', verticalAlign: 'sub', marginRight: 10}}/>
-                            Send me a message!
-                        </h1>
-                        <ContactSection/>
-                    </div>
-                    <div>
-                        Email Address: johnpaoloflores@gmail.com
-                    </div>
                 </div>
+                
+
+                <Parallax
+                    bgImage={BG3}
+                    bgImageAlt="the 2nd bg"
+                    strength={300}
+                >
+                    <div style={{display:"flex", justifyContent:'center', padding: "50px 0"}}>
+                        <div style={{marginRight: 30}}>
+                            <h1 className="header">
+                                <GoMail size={40} style={{color:'#BDBDBD', verticalAlign: 'sub', marginRight: 10}}/>
+                                Send me a message!
+                            </h1>
+                            <ContactSection/>
+                        </div>
+                        <div style={{display:'flex', alignItems:'center', marginLeft: 100}}>
+                            <Button style={{height: 75}} onClick={onLinkedInClick.bind(this)}>
+                                <FaLinkedin size={75}/>
+                            </Button>
+                            <Button style={{height: 75}} onClick={onGitHubClicked.bind(this)}>
+                                <SiGithub size={75}/>
+                            </Button>
+                        </div>
+                    </div>
+                </Parallax>
             </div>
         </div>
     )
--- web/src/components/projectitem.js
+++ web/src/components/projectitem.js
@@ -2,15 +2,14 @@ import { Link } from "@material-ui/core"
 import React from "react"
 
 const ProjectItem = (props) => {
-    // const {previewPicture, projectTitle, projectDescription, projectLink} = props
+     const {previewPicture, projectTitle, projectDescription, projectLink} = props
 
     return <div style={{display:"flex", padding: 20}}>
-        <img src={props.previewPicture} alt="my pic" style={{height: 300, width:500, flexShrink:0}}/>
+        <img src={previewPicture} alt="my pic" style={{height: 300, width:500, flexShrink:0}}/>
         <div style={{marginLeft: 30, color: "white", display:"flex", flexDirection:"column", justifyContent:"center"}}>
-            <h1>{props.projectTitle}</h1>
-            <h4>{props.projectDescription}</h4>
-            <Link href={props.projectLink}>Go to site</Link>
-            {/* <h5>{props.projectLink}</h5> */}
+            <h1>{projectTitle}</h1>
+            <h4>{projectDescription}</h4>
+            <Link href={projectLink}>Go to site</Link>
         </div>
     </div>
 }
